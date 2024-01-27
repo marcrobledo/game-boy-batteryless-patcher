6de08e7ba8a2c4d29d9ae7a8f2afdf1952e3a8d1
Marc Robledo <mark@tinet.org>
2024-01-27 23:00:56 +0100

    added support for games that store current bank in SRAM instead of RAM

--- src/main.asm
+++ src/main.asm
@@ -46,8 +46,10 @@ ENDC
 ; define section and label for game's current bank byte
 IF GAME_ENGINE_CURRENT_BANK_OFFSET >= _HRAM
 	SECTION "HRAM - original game's bank switch backup", HRAM[GAME_ENGINE_CURRENT_BANK_OFFSET]
+ELIF GAME_ENGINE_CURRENT_BANK_OFFSET >= _RAM
+	SECTION "WRAM - original game's bank switch backup", WRAM0[GAME_ENGINE_CURRENT_BANK_OFFSET]
 ELSE
-	SECTION "RAM - original game's bank switch backup", WRAM0[GAME_ENGINE_CURRENT_BANK_OFFSET]
+	SECTION "SRAM - original game's bank switch backup", SRAM[GAME_ENGINE_CURRENT_BANK_OFFSET], BANK[0]
 ENDC
 
 _current_game_bank:
